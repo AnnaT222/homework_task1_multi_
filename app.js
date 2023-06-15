ba1e05f77279eec4ae4f6b5780706ba0d617764a
AnnaT222 <annatazaian@gmail.com>
2023-06-15 22:38:55 +0400

    changes done

--- app.js
+++ app.js
@@ -1,36 +1,58 @@
 import { spawn } from 'child_process';
 import fs from 'fs';
 
-function getStatistics(command, args, timeout) {
-    const startTime = new Date();
-    const timestamp = startTime.toISOString().replace(/:/g, '-');
-    spawn('mkdir', ['./logs']);
-    const statistics = {
-        start: timestamp,
-        duration: null,
-        success: false,
-        commandSuccess: true,
-        error: "There isn't any errors"
+const fsPromises = fs.promises
 
-    }
+const statistics = {
+    start: getTime(),
+    duration: null,
+    success: false,
+    commandSuccess: true,
+    error: null
+}
+
+fsPromises.mkdir('./logs').then(() => {
+    console.log('Directory created successfully');
+}).catch(() => {
+    console.log('Already created');
+})
+async function getStatistics(command, args, timeout) {
+    const fileName = `${statistics.start}_${command}.json`
+
     const childProcess = spawn(command, args);
     childProcess.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
     });
-    childProcess.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
+
+
+    childProcess.on('error', (data) => {
+        console.log(`error: ${data}`);
     });
+
+
     childProcess.on('error', (error) => {
         statistics.error = error.message
         statistics.commandSuccess = false
         console.error('Child: Command execution failed');
     });
+
     childProcess.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
         if (statistics.commandSuccess) {
+            getDuration()
+            statistics.success = true
             delete statistics.commandSuccess
+        } else {
+            getDuration()
         }
-        fs.createWriteStream('./logs/' + `${timestamp}.json`).write(JSON.stringify(statistics));
+        fs.createWriteStream('./logs/' + `${fileName}`).write(JSON.stringify(statistics));
     });
 }
-getStatistics('ls', ["-lh"])
+function getTime() {
+    const start = Date.now()
+    return start
+}
+function getDuration() {
+    statistics.duration = getTime() - statistics.start
+}
+await getStatistics('ls4', ["-lh"])
